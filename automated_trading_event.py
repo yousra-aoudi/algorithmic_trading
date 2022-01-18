56003cdd17b1a024a49a5630843f8625e34845d2
yousra aoudi <97890475+yousra-aoudi@users.noreply.github.com>
2022-01-18 16:52:22 +0000

    Update automated_trading_event.py

--- automated_trading_event.py
+++ automated_trading_event.py
@@ -5,6 +5,7 @@
 
 # event.py
 from __future__ import print_function
+
 class Event(object): 
   """
   Event is base class providing an interface for all subsequent (inherited) events, that will trigger further events in the
@@ -12,9 +13,9 @@ class Event(object):
   pass
 
 #MarketEvent
+
 #MarketEvents are triggered when the outer while loop of the backtesting system begins a new "heartbeat".
 
-# event.py
 class MarketEvent(Event): 
   """
   Handles the event of receiving a new market update with
@@ -27,11 +28,11 @@ class MarketEvent(Event):
      self.type = ’MARKET’
     
 #SignalEvent
+
 #The Strategy object utilises market data to create new SignalEvents. 
 #The SignalEvent contains a strategy ID, a ticker symbol, a timestamp for when it was generated, a direction (long or short) 
 #and a "strength" indicator (this is useful for mean reversion strategies).
 
-# event.py
 class SignalEvent(Event): 
   """
   Handles the event of sending a Signal from a Strategy object. This is received by a Portfolio object and acted upon.
@@ -57,16 +58,17 @@ class SignalEvent(Event):
 
  
 #OrderEvent
+
 #When a Portfolio object receives SignalEvents it assesses them in the wider context of the portfolio, in terms of risk and position sizing. 
 #This ultimately leads to OrderEvents that will be sent to an ExecutionHandler.
 
-# event.py
 class OrderEvent(Event): 
   """
   Handles the event of sending an Order to an execution system.
   The order contains a symbol (e.g. GOOG), a type (market or limit),
   quantity and a direction.
   """
+  
   def __init__(self, symbol, order_type, quantity, direction):
     """
     Initialises the order type, setting whether it is
@@ -94,11 +96,11 @@ class OrderEvent(Event):
          )
     
 #FillEvent
+
 #When an ExecutionHandler receives an OrderEvent it must transact the order. 
 #Once an order has been transacted it generates a FillEvent, 
 #which describes the cost of purchase or sale as well as the transaction costs, such as fees or slippage.
 
-# event.py
 class FillEvent(Event): 
   """
   Encapsulates the notion of a Filled Order, as returned
@@ -106,6 +108,7 @@ class FillEvent(Event):
   actually filled and at what price. In addition, stores
   the commission of the trade from the brokerage.
   """
+  
   def __init__(self, timeindex, symbol, exchange, quantity, 
                direction, fill_cost, commission=None):
     """
@@ -129,239 +132,27 @@ class FillEvent(Event):
     self.quantity = quantity
     self.direction = direction
     self.fill_cost = fill_cost
+    
     # Calculate commission
     if commission is None:
       self.commission = self.calculate_ib_commission()
     else:
       self.commission = commission
  
-def calculate_ib_commission(self):
-  """
-  Calculates the fees of trading based on an Interactive
-  Brokers fee structure for API, in USD.
-  This does not include exchange or ECN fees.
-  Based on "US API Directed Orders": https://www.interactivebrokers.com/en/index.php? f=commission&p=stocks2
-  """
-  full_cost = 1.3
-  if self.quantity <= 500:
-    full_cost = max(1.3, 0.013 * self.quantity) 
-  else: 
-    # Greater than 500
-    full_cost = max(1.3, 0.008 * self.quantity) 
-  return full_cost
-
-#Data Handler
-#One of the goals of an event-driven trading system is to minimise duplication of code between the backtesting element and the live execution element.
-
-# data.py
-from __future__ import print_function
-
-from abc import ABCMeta, abstractmethod 
-import datetime
-import os, os.path
-
-import numpy as np 
-import pandas as pd
-
-from event import MarketEvent
-
-# data.py
-
-class DataHandler(object): 
-  """
-  DataHandler is an abstract base class providing an interface for all subsequent (inherited) data handlers (both live and historic).
-  The goal of a (derived) DataHandler object is to output a generated set of bars (OHLCVI) for each symbol requested.
-  This will replicate how a live strategy would function as current
-  market data would be sent "down the pipe". Thus a historic and live system will be treated identically by the rest of the backtesting suite. 
-  """
-  __metaclass__ = ABCMeta
-  
-  @abstractmethod
-  def get_latest_bar(self, symbol):
+  def calculate_ib_commission(self):
     """
-    Returns the last bar updated.
+    Calculates the fees of trading based on an Interactive
+    Brokers fee structure for API, in USD.
+    This does not include exchange or ECN fees.
+    Based on "US API Directed Orders": https://www.interactivebrokers.com/en/index.php? f=commission&p=stocks2
     """
-    raise NotImplementedError("Should implement get_latest_bar()")
-  
-  @abstractmethod
-  def get_latest_bars(self, symbol, N=1):
-    """
-    Returns the last N bars updated.
-    """
-    raise NotImplementedError("Should implement get_latest_bars()")
+    full_cost = 1.3
     
-  @abstractmethod
-  def get_latest_bar_datetime(self, symbol):
-    """
-    Returns a Python datetime object for the last bar. 
-    """
-    raise NotImplementedError("Should implement get_latest_bar_datetime()")
-  
-  @abstractmethod 
-  def get_latest_bar_value(self, symbol, val_type): 
-    """
-    Returns one of the Open, High, Low, Close, Volume or OI from the last bar.
-    """
-    raise NotImplementedError("Should implement get_latest_bar_value()")
-  
-  @abstractmethod
-  def get_latest_bars_values(self, symbol, val_type, N=1):
-    """
-    Returns the last N bar values from the
-    latest_symbol list, or N-k if less available. 
-    """
-    raise NotImplementedError("Should implement get_latest_bars_values()")
+    if self.quantity <= 500:
+      full_cost = max(1.3, 0.013 * self.quantity) 
+    else: 
+      # Greater than 500
+      full_cost = max(1.3, 0.008 * self.quantity) 
     
-  @abstractmethod
-  def update_bars(self):
-    """
-    Pushes the latest bars to the bars_queue for each symbol
-    in a tuple OHLCVI format: (datetime, open, high, low,
-    close, volume, open interest).
-    """
-    raise NotImplementedError("Should implement update_bars()")
+    return full_cost
 
-# data.py
-class HistoricCSVDataHandler(DataHandler):
-  """
-  HistoricCSVDataHandler is designed to read CSV files for each requested symbol from disk and provide an interface to obtain the "latest" bar in a manner identical to a live trading interface.
-  """
-  def __init__(self, events, csv_dir, symbol_list): 
-    """
-    Initialises the historic data handler by requesting the location of the CSV files and a list of symbols.
-    It will be assumed that all files are of the form ’symbol.csv’, where symbol is a string in the list.
-    Parameters:
-    events - The Event Queue.
-    csv_dir - Absolute directory path to the CSV files.
-    symbol_list - A list of symbol strings.
-    """
-    self.events = events
-    self.csv_dir = csv_dir
-    self.symbol_list = symbol_list
-    self.symbol_data = {}
-    self.latest_symbol_data = {}
-    self.continue_backtest = True
-    self._open_convert_csv_files()
-    
-  # data.py
-  def _open_convert_csv_files(self): 
-    """
-    Opens the CSV files from the data directory, converting
-    them into pandas DataFrames within a symbol dictionary.
-    For this handler it will be assumed that the data is taken from Yahoo. Thus its format will be respected. 
-    """
-    comb_index = None
-    for s in self.symbol_list:
-      # Load the CSV file with no header information, indexed on date 
-      self.symbol_data[s] = pd.io.parsers.read_csv(os.path.join(self.csv_dir, ’%s.csv’ % s), header=0, index_col=0, parse_dates=True,
-                                                 names=[’datetime’, ’open’, ’high’,’low’, ’close’, ’volume’, ’adj_close’]).sort()
-      # Combine the index to pad forward values
-      if comb_index is None:
-        comb_index = self.symbol_data[s].index
-      else: 
-        comb_index.union(self.symbol_data[s].index)
-      # Set the latest symbol_data to None
-      self.latest_symbol_data[s] = []
-      # Reindex the dataframes
-      for s in self.symbol_list:
-        self.symbol_data[s] = self.symbol_data[s].\
-        reindex(index=comb_index, method=’pad’).iterrows()
-        
-    # data.py
-    def _get_new_bar(self, symbol): 
-      """
-      Returns the latest bar from the data feed. 
-      """
-      for b in self.symbol_data[symbol]:
-        yield b
-    
-    # data.py
-    def get_latest_bar(self, symbol): 
-      """
-      Returns the last bar from the latest_symbol list.
-      """ 
-      try:
-        bars_list = self.latest_symbol_data[symbol] 
-        except KeyError:
-          print("That symbol is not available in the historical data set.") 
-          raise
-        else:
-          return bars_list[-1]
-     def get_latest_bars(self, symbol, N=1): 
-      """
-      Returns the last N bars from the latest_symbol list,
-      or N-k if less available. 
-      """
-      try:
-        bars_list = self.latest_symbol_data[symbol] 
-      except KeyError:
-        print("That symbol is not available in the historical data set.")
-        raise 
-      else:
-        return bars_list[-N:]
-      
-     #get_latest_bar_datetime, queries the latest bar for a datetime object representing the "last market price"
-    def get_latest_bar_datetime(self, symbol): 
-      """
-      Returns a Python datetime object for the last bar. 
-      """
-      try:
-        bars_list = self.latest_symbol_data[symbol]
-        except KeyError:
-          print("That symbol is not available in the historical data set.") 
-          raise
-        else:
-          return bars_list[-1][0]
-     
-    #get_latest_bar_value and get_latest_bar_value
-    def get_latest_bar_datetime(self, symbol): 
-      """
-      Returns a Python datetime object for the last bar. 
-      """
-      try:
-        bars_list = self.latest_symbol_data[symbol]
-      except KeyError:
-        print("That symbol is not available in the historical data set.") 
-        raise
-      else:
-        return bars_list[-1][0]
-     
-    def get_latest_bar_value(self, symbol, val_type): 
-      """
-      Returns one of the Open, High, Low, Close, Volume or OI
-      values from the pandas Bar series object.
-      """
-      try:
-        bars_list = self.latest_symbol_data[symbol]
-      except KeyError:
-        print("That symbol is not available in the historical data set.")
-        raise 
-      else:
-        return getattr(bars_list[-1][1], val_type)
-    
-    def get_latest_bars_values(self, symbol, val_type, N=1): 
-      """
-      Returns the last N bar values from the latest_symbol list, or N-k if less available. 
-      """
-      try:
-        bars_list = self.get_latest_bars(symbol, N) 
-      except KeyError:
-        print("That symbol is not available in the historical data set.")
-        raise 
-      else:
-        return np.array([getattr(b[1], val_type) for b in bars_list])
-  
-    # data.py
-    def update_bars(self): 
-      """
-      Pushes the latest bar to the latest_symbol_data structure for all symbols in the symbol list.
-      """
-      for s in self.symbol_list: 
-        try:
-          bar = next(self._get_new_bar(s)) 
-        except StopIteration:
-            self.continue_backtest = False 
-        else:
-          if bar is not None: 
-            self.latest_symbol_data[s].append(bar)
-            self.events.put(MarketEvent())
