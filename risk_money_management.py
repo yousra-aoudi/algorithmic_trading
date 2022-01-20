aec639547f73ae212fe376cc42925627431b3e5d
yousra aoudi <97890475+yousra-aoudi@users.noreply.github.com>
2022-01-20 21:54:22 +0000

    Update risk_money_management.py

--- risk_money_management.py
+++ risk_money_management.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 import datetime
 import numpy as np
-import pandas.io.data as web 
+import pandas_datareader as web 
 
 from scipy.stats import norm
 
@@ -17,7 +17,7 @@ def var_cov_var(P, c, mu, sigma):
   return P - P*(alpha + 1)
 
 if __name__ == "__main__":
-  start = datetime.datetime(2010, 1, 1) end = datetime.datetime(2014, 1, 1)
+  start = datetime.datetime(2021, 1, 1) end = datetime.datetime(2022, 1, 1)
   citi = web.DataReader("C", ’yahoo’, start, end)
   citi["rets"] = citi["Adj Close"].pct_change()
   P = 1e6 # 1,000,000 USD
